5cd4fcce8bb32f3766f3c3533282d562f6bbc2a9
Danil <danil.polovinkin.1994@gmail.com>
2023-09-13 15:01:06 +0300

    created loading comp,some firebase logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, Suspense } from 'react';
 import './components/styles/index.scss';
 import { Routes, Route } from 'react-router-dom';
+import Loading from 'react-loading';
 
 const Registration = React.lazy(() => import('./components/pages/Registration/Registration'));
 
 const App: FC = () => {
     return (
-        <Routes>
-            {/* Header */}
-            <Route path='/registration/*' element={<Registration />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+            <Routes>
+                {/* Header */}
+                <Route path='/registration/*' element={<Registration />} />
+            </Routes>
+        </Suspense>
     );
 };
 
