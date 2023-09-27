7a5e7f29a954f2e8b702a8e6c5e75c018a2c1bd7
Danil <danil.polovinkin.1994@gmail.com>
2023-09-27 14:59:42 +0300

    completed some of its logic

--- src/components/pages/Creator/AddVideo1/AddVideo1.tsx
+++ src/components/pages/Creator/AddVideo1/AddVideo1.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 import './AddVideo.scss';
 import Button from '../../../ui/Button/Button';
 import pngLogoW from '../../../../assets/img/addvideo/addvideo.png?as=webp';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { DB, storage } from '../../../../config/firebase-config';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
 import { useNavigate } from 'react-router-dom';
@@ -16,21 +16,22 @@ import pngPicW from '../../../../assets/img/addvideo/addvideo-white.png?as=webp'
 interface IObject {
     name: string;
     date: number;
-    prevew: File | null;
+    preview: File | null;
     category: string;
     description: string;
 }
 
-const AddVideo1 = () => {
+const AddVideo1: FC = () => {
     const [video, setVideo] = useState<File | null>(null);
     const [videoTitle, setVideoTitle] = useState<string>('');
+    const [videoDescr, setVideoDescr] = useState<string>('');
     const [videoType, setVideoType] = useState<string | number>('');
-    const [prevew, setPreview] = useState<File | null>(null);
+    const [shopify, setShopify] = useState<string>('');
+    const [preview, setPreview] = useState<File | null>(null);
 
     const videoHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             setVideo(e.target.files[0]);
-            console.log(e.target.files[0]);
         }
     };
 
@@ -43,13 +44,12 @@ const AddVideo1 = () => {
             return;
         }
         const filesFolderRef = ref(storage, `videos/${selector.regData.email}/${videoTitle}`);
-        const res = await updateDoc(doc(DB, 'users', selector.regData.email), {
-            videos: [{ name: 'shrekorabbit' }, { name: 'waterfall' }],
-        });
+        const previewsFolderRef = ref(storage, `previews/${selector.regData.email}/${videoTitle}`);
 
         try {
-            const res = await uploadBytes(filesFolderRef, video);
-            console.log(res);
+            const videoRes = await uploadBytes(filesFolderRef, video);
+            const previewRes = await uploadBytes(previewsFolderRef, video);
+            console.log(videoRes, previewRes);
         } catch (e) {
             alert(e);
         }
@@ -59,15 +59,25 @@ const AddVideo1 = () => {
         <div className='addvideo'>
             <div className='addvideo__top'>
                 <p className='addvideo__text'>Adding a new video</p>
-                <Button className='addvideo__btn btn-innactive'>Continue</Button>
+                <Button
+                    className='addvideo__btn btn-innactive'
+                    onClickHandler={async () => {
+                        const oldData = await getDoc(doc(DB, 'users', selector.regData.email)).then((res) => res.data()?.videos);
+                        const res = await updateDoc(doc(DB, 'users', selector.regData.email), {
+                            videos: [...oldData, { title: videoTitle, descr: videoDescr, category: videoType, shopify }],
+                        });
+                        await uploadVideo();
+                        // console.log(oldData);
+                    }}
+                >
+                    Publish
+                </Button>
             </div>
             <div className='addvideo__dragAndDrop-section'>
                 <input type='file' onChange={videoHandler} className='addvideo__input' />
                 <img src={pngLogoW} className='addvideo__img' />
                 <p className='addvideo__drag-text'>Drag and drop videos to upload</p>
-                <Button className='addvideo__btn-addvideo' onClickHandler={() => uploadVideo()}>
-                    Or choose files
-                </Button>
+                <Button className='addvideo__btn-addvideo'>Or choose files</Button>
             </div>
             <div className='addvideo__bottom'>
                 <div className='addvideo__bottom__left'>
@@ -86,14 +96,39 @@ const AddVideo1 = () => {
                         placeholder='Select category'
                         title='Category'
                     />
-                    <InputContainer className='addvideo__bottom__input' text='Description' placeholder='Description' />
-                    <InputContainer className='addvideo__bottom__input' text='Add Shopify link' placeholder='Add link on product' />
+                    <InputContainer
+                        className='addvideo__bottom__input'
+                        text='Description'
+                        placeholder='Description'
+                        value={videoDescr}
+                        onChangeHandler={(e) => setVideoDescr(e.target.value)}
+                    />
+                    <InputContainer
+                        className='addvideo__bottom__input'
+                        text='Add Shopify link'
+                        placeholder='Add link on product'
+                        onChangeHandler={(e) => setShopify(e.target.value)}
+                    />
                 </div>
                 <div className='addvideo__bottom__right'>
-                    <input type='file' onChange={videoHandler} className='addvideo__input' />
-                    <img src={pngLogoW} className='addvideo__right_img' />
-                    <p className='addvideo__right_text'>Drag and drop photo to upload</p>
-                    <p className='addvideo__right_descr'>Information about adding photo. Amet minim mollit non deserunt ullamco est sit </p>
+                    <input
+                        type='file'
+                        onChange={(e) => {
+                            if (e.target.files) {
+                                setPreview(e.target.files[0]);
+                            }
+                        }}
+                        className='addvideo__input'
+                    />
+                    {preview ? (
+                        <img src={URL.createObjectURL(preview)} className='addvideo__preview' />
+                    ) : (
+                        <>
+                            <img src={pngLogoW} className='addvideo__right_img' />
+                            <p className='addvideo__right_text'>Drag and drop photo to upload</p>
+                            <p className='addvideo__right_descr'>Information about adding photo. Amet minim mollit non deserunt ullamco est sit </p>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
