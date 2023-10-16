bd21eb4f424137c16b37200cd4deabfb6417a360
Danil <danil.polovinkin.1994@gmail.com>
2023-10-16 16:24:56 +0300

    some drafts on banner

--- src/components/pages/User/UserChannel/Tabs/Tabs.tsx
+++ src/components/pages/User/UserChannel/Tabs/Tabs.tsx
@@ -5,25 +5,22 @@ import { useAppSelector } from '../../../../hooks/redux';
 import { v1 } from 'uuid';
 
 const Tabs: FC = ({}) => {
-    // const [tabs, setTabs] = useState([true, false, false, false]);
+    const [tabs, setTabs] = useState([true, false, false]);
 
-    // const selectorEmail = useAppSelector((state) => state.creatorSlice.creatorEmail.email);
-    // const selectorUsers = useAppSelector((state) => state.usersSlice.data);
-    // const [channelData, setChannelData] = useState(selectorUsers.filter((el) => el.email === selectorEmail));
+    const selectorEmail = useAppSelector((state) => state.creatorSlice.creatorEmail.email);
+    const selectorUsers = useAppSelector((state) => state.usersSlice.data);
+    const [channelData, setChannelData] = useState(selectorUsers.filter((el) => el.email === selectorEmail));
 
     return (
         <div className='tabs'>
-            {/* <div className='tabs__items'>
-                <p className={`${tabs[0] ? 'active' : ''} tab__item `} onClick={() => setTabs([true, false, false, false])}>
+            <div className='tabs__items'>
+                <p className={`${tabs[0] ? 'active' : ''} tab__item `} onClick={() => setTabs([true, false, false])}>
                     Video
                 </p>
-                <p className={`${tabs[1] ? 'active' : ''} tab__item `} onClick={() => setTabs([false, true, false, false])}>
+                <p className={`${tabs[1] ? 'active' : ''} tab__item `} onClick={() => setTabs([false, true, false])}>
                     Bio
                 </p>
-                <p className={`${tabs[2] ? 'active' : ''} tab__item `} onClick={() => setTabs([false, false, true, false])}>
-                    Store
-                </p>
-                <p className={`${tabs[3] ? 'active' : ''} tab__item `} onClick={() => setTabs([false, false, false, true])}>
+                <p className={`${tabs[2] ? 'active' : ''} tab__item `} onClick={() => setTabs([false, false, true])}>
                     Playlists
                 </p>
             </div>
@@ -31,9 +28,13 @@ const Tabs: FC = ({}) => {
                 <div className={`${tabs[0] ? 'active' : ''} tabs__videos`}>
                     {channelData[0].videos ? channelData[0].videos.map((el) => <Video key={v1()} title={el.title} previewUrl={el.previewUrl}></Video>) : ''}
                 </div>
-                <div className={`${tabs[1] ? 'active' : ''} tabs__bio`}></div>
-                <div className='tabs__playlists'></div>
-            </div> */}
+                <div className={`${tabs[1] ? 'active' : ''} tabs__bio`}>
+                    <div>hey</div>
+                </div>
+                <div className={`${tabs[1] ? 'active' : ''} tabs__playlist`}>
+                    <div>hey2</div>
+                </div>
+            </div>
         </div>
     );
 };
